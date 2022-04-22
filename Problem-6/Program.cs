05ecd30c5db92230b6dd1630ae6633a7786c4aec
BerkTolay <berktolay@outlook.com>
2022-04-22 20:12:05 +0300

    Project Euler 6

---
+++ Problem-6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, sumSquare = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sum += i;
+                sumSquare += i * i;
+            }
+            Console.WriteLine((Math.Pow(sum, 2)) - sumSquare);
+        }
+    }
+}
