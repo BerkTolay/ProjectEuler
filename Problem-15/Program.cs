dd4b74b95b2cf97844a00367d6ebadd1af4d66af
BerkTolay <berktolay@outlook.com>
2022-05-01 19:56:18 +0300

    Problem 15

---
+++ Problem-15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+namespace Problem_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger row = 20;
+            BigInteger column = 20;
+            BigInteger result = Fact(row + column) / (Fact(row) * Fact(column));
+            Console.WriteLine(result);
+        }
+        public static BigInteger Fact(BigInteger number)
+        {
+            BigInteger sum = 1;
+            while (number > 0)
+            {
+                sum *= number;
+                number--;
+            }
+            return sum;
+        }
+    }
+}
