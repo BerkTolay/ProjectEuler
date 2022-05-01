24da01b2d253a572adf2ed24c3d81ab16ab45ae6
BerkTolay <berktolay@outlook.com>
2022-05-01 19:57:41 +0300

    Problem 16

---
+++ Problem-16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Pow(2, 1000);
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += (int)(number % 10);
+                number /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
